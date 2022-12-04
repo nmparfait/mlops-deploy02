94a1d50bf2867202a35300e2590f61c5aa165724
nmparfait <mbamu.developer@gmail.com>
2022-12-03 22:20:02 -0500

    mlops deploy with cookiebetter step 01

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mlops deploy of the model built with flask and machine learning algorithme',
+    author='nmparfait',
+    license='',
+)
